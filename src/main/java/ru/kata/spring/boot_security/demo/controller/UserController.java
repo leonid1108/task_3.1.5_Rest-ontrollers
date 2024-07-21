59970cee5b995251e46db6b5fecb59c938f083f7
leonid1108 <luschnikov.lyonya@yandex.ru>
2024-07-21 19:02:54 +0400

    Edit commit

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,15 +1,30 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import ru.kata.spring.boot_security.demo.entities.User;
+import ru.kata.spring.boot_security.demo.security.UserDetailService;
 
 @Controller
 public class UserController {
 
+    private final UserDetailService userDetailService;
+
+    @Autowired
+    public UserController(UserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
     @GetMapping("/user")
     public String userPage(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        User user = userDetailService.findByUsername(username);
+        model.addAttribute("user", user);
         return "user";
     }
 }
